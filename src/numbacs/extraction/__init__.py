5be96f40e2a6c0fb03ae19475293ef60271d344e
alb3rtjarvis <ajarvis@vt.edu>
2024-09-23 13:47:16 -0400

    remove version and author

--- src/numbacs/extraction/__init__.py
+++ src/numbacs/extraction/__init__.py
@@ -1,8 +1,6 @@
 """
 Extraction package for numbacs
 """
-__author__ = """Albert Jarvis"""
-__version__ = '0.1.0'
 
 from .ridges import ftle_ridge_pts, ftle_ridges, ftle_ordered_ridges
 from .hyperbolic import hyperbolic_lcs, hyperbolic_oecs
